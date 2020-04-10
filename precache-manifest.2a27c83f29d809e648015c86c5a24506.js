f96ffb168c6a54f7b8c43f9d97b3191bad0fd7e3
Lakshan Maduranga <lakshanmadhuranga77@gmail.com>
2020-04-10 15:00:58 +0530

    Updates

---
+++ precache-manifest.2a27c83f29d809e648015c86c5a24506.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5ce61d1cc685ef1f8f4ab8e60de40c",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "415039111b84629f3d3d",
+    "url": "/CoronaTracker/static/css/main.3008dee6.chunk.css"
+  },
+  {
+    "revision": "7f26502e134cb9f0f55b",
+    "url": "/CoronaTracker/static/js/2.31522fd5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CoronaTracker/static/js/2.31522fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "415039111b84629f3d3d",
+    "url": "/CoronaTracker/static/js/main.d309eaf9.chunk.js"
+  },
+  {
+    "revision": "1240089099cedbec8904",
+    "url": "/CoronaTracker/static/js/runtime-main.521f1356.js"
+  },
+  {
+    "revision": "04a5da4d268e0579d4da37279e5d518b",
+    "url": "/CoronaTracker/static/media/unnamed.04a5da4d.jpg"
+  }
+]);
